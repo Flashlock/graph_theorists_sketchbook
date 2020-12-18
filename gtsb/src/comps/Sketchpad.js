d78c50261b8ad2423c1cab3b21439bc056024fc9
Flashlock <ajamesbetts1998@gmail.com>
2020-12-17 16:31:18 -0800

    I can select Vertices

--- gtsb/src/comps/Sketchpad.js
+++ gtsb/src/comps/Sketchpad.js
@@ -4,43 +4,18 @@
 // eslint-disable-next-line no-undef
 const e = React.createElement;
 
-//vertex element
-function VertexRenderer(vertex) {
-  const vertexDiameter=25;
-  const style = {
-    position: 'absolute',
-    top: vertex.y-vertexDiameter/2,
-    left: vertex.x-vertexDiameter/2,
-    background: 'blue',
-    borderRadius: '50%',
-    width: vertexDiameter+'px',
-    height: vertexDiameter+'px'
-  }
-  const id = 'v' + vertex.id.toString();
-  return e(
-      'div',
-      {
-        style: style,
-        id: id,
-        key: id
-      }
-  );
-}
-
-//edge element
-const EdgeRenderer = (props) =>{
-  return null;
-}
-
 // eslint-disable-next-line no-undef
 class Sketchpad extends React.Component {
   constructor(props) {
     super(props);
-    this.state={};
+    this.state = {};
     this.vertices = [];
     this.edges = [];
+    this.selectedVertices = [];
     this.vertexIDCount = 0;
     this.edgeIDCount = 0;
+    this.canDrawVertex = true;
+    this.selectionBorderRadius=4;
   }
 
   render() {
@@ -53,7 +28,7 @@ class Sketchpad extends React.Component {
         [
           //map each vertex to its dom equivalent
           this.vertices.map((vertex) => {
-            return VertexRenderer(vertex);
+            return this.VertexRenderer(vertex);
               }
           ),
 
@@ -66,13 +41,72 @@ class Sketchpad extends React.Component {
     );
   }
 
-  drawVertex = (e) =>{
-    const vertex={
+  //vertex element
+  VertexRenderer = (vertex)=> {
+    const vertexDiameter = 25;
+    const x = vertex.isSelected ? vertex.x - vertexDiameter / 2 - this.selectionBorderRadius : vertex.x - vertexDiameter / 2;
+    const y = vertex.isSelected ? vertex.y - vertexDiameter / 2 - this.selectionBorderRadius : vertex.y - vertexDiameter / 2;
+    const style = {
+      position: 'absolute',
+      top: y,
+      left: x,
+      background: vertex.isHovering ? 'pink' : 'blue',
+      borderRadius: '50%',
+      border: vertex.isSelected ? this.selectionBorderRadius + 'px solid pink' : null,
+      width: vertexDiameter + 'px',
+      height: vertexDiameter + 'px'
+    }
+    const id = 'v' + vertex.id.toString();
+
+    return e(
+        'div',
+        {
+          style: style,
+          id: id,
+          key: id,
+          onClick: this.selectElement.bind(this, true, vertex),
+          onMouseEnter: this.mouseEnterElement.bind(this, true, vertex),
+          onMouseLeave: this.mouseExitElement.bind(this, true, vertex)
+        }
+    );
+  }
+
+//edge element
+  EdgeRenderer = (props) =>{
+    return null;
+  }
+
+  drawVertex = (e) => {
+    if (!this.canDrawVertex)
+      return;
+    const vertex = {
       id: this.vertexIDCount++,
       x: e.clientX,
       y: e.clientY
     }
     this.vertices.push(vertex);
+    this.setState(this.state);
+  }
+
+  selectElement=(isVertex, element)=> {
+    if (isVertex) {
+      if (!this.selectedVertices.find((vertex) => vertex.id === element.id)) {
+        this.selectedVertices.push(element);
+        element.isSelected = true;
+      }
+    }
+    this.setState(this.state);
+  }
+
+  mouseEnterElement=(isVertex, element)=> {
+    this.canDrawVertex = false;
+    element.isHovering = true;
+    this.setState(this.state);
+  }
+
+  mouseExitElement=(isVertex, element)=> {
+    this.canDrawVertex = true;
+    element.isHovering = false;
     this.setState(this.state);
   }
 }
